892b3b054f25ebd6f9f9fd8242fb38d170fcb449
reevchris100 <reevchris100@gmail.com>
2016-07-25 23:28:03 +0530

    New changes

---
+++ sidebar.php
@@ -0,0 +1,37 @@
+ <div class="contact">
+                        <ul>
+                            <li class="address">
+                                <span>Address</span>
+                                <ul>
+                                    <li>
+									<i class = "icon icon-home"></i>
+									<p>Bangalore </p>
+									<p>Karnataka,India</p>
+                                    </li>
+                                  
+                                
+                                </ul>
+                            </li>
+                            <li class="contactInfo">
+								
+									<i class = "icon icon-user"></i>
+                                <ul>
+                                    <li>
+                                        +919535797323
+                                    </li>
+                                    <li>
+                                        +919535797323
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="mail">
+                                <i class = "icon icon-envelope"> </i>
+                                <ul>
+                                    <li>
+                                        <a href="#">shopncart@gmail.com</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                   
---
+++ user_about.php
@@ -0,0 +1,176 @@
+<?php include('header.php'); ?>
+<?php include('session.php'); ?>
+<?php include 'admin/connect.php'; ?>
+<body>
+    <?php 
+    include('navtop.php');
+    ?>
+    <div id="background">
+        <?php
+        include ('navbarfixed.php');
+        ?>
+
+        <div id="page">
+           <?php include ('nav_sidebar2.php');?>
+            <div id="content">
+                <div class="hero-unit-table">
+                    <div id="header">
+                        
+
+                    </div>
+                    <div id="body">
+
+
+
+
+
+
+
+
+                        <h3>About US</h3>
+                        <div class="hero-unit-table">
+
+                            	<div id="accordion">
+										<h3>About The Company</h3>
+										<div>Shop N Cart is an online portal for the sale of items like bags for travelling ,school and other purposes.  </div>
+										<h3>Mission</h3>
+										<div>Shop N Cart- an outlet that provide individuals the satisfaction of owning a wide range of high 
+											quality and reliable items.</div>
+										<h3>Vision</h3>
+										<div>Our items will never ever disappoint you.</div>
+										<h3>About the Developer</h3>
+										<div>Reevan Chris Pinto is a 3rd Year B.E. C.S.E student of UVCE.</div>
+							</div>
+                        </div>
+                    </div>
+                    <div id="footer">
+                        <?php include('footer_user.php'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include('footer_bottom.php'); ?>
+		<script src="external/jquery/jquery.js"></script>
+<script src="jquery-ui.js"></script>
+<script>
+
+$( "#accordion" ).accordion();
+
+
+
+var availableTags = [
+	"ActionScript",
+	"AppleScript",
+	"Asp",
+	"BASIC",
+	"C",
+	"C++",
+	"Clojure",
+	"COBOL",
+	"ColdFusion",
+	"Erlang",
+	"Fortran",
+	"Groovy",
+	"Haskell",
+	"Java",
+	"JavaScript",
+	"Lisp",
+	"Perl",
+	"PHP",
+	"Python",
+	"Ruby",
+	"Scala",
+	"Scheme"
+];
+$( "#autocomplete" ).autocomplete({
+	source: availableTags
+});
+
+
+
+$( "#button" ).button();
+$( "#radioset" ).buttonset();
+
+
+
+$( "#tabs" ).tabs();
+
+
+
+$( "#dialog" ).dialog({
+	autoOpen: false,
+	width: 400,
+	buttons: [
+		{
+			text: "Ok",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		},
+		{
+			text: "Cancel",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	]
+});
+
+// Link to open the dialog
+$( "#dialog-link" ).click(function( event ) {
+	$( "#dialog" ).dialog( "open" );
+	event.preventDefault();
+});
+
+
+
+$( "#datepicker" ).datepicker({
+	inline: true
+});
+
+
+
+$( "#slider" ).slider({
+	range: true,
+	values: [ 17, 67 ]
+});
+
+
+
+$( "#progressbar" ).progressbar({
+	value: 20
+});
+
+
+
+$( "#spinner" ).spinner();
+
+
+
+$( "#menu" ).menu();
+
+
+
+$( "#tooltip" ).tooltip();
+
+
+
+$( "#selectmenu" ).selectmenu();
+
+
+// Hover states on the static widgets
+$( "#dialog-link, #icons li" ).hover(
+	function() {
+		$( this ).addClass( "ui-state-hover" );
+	},
+	function() {
+		$( this ).removeClass( "ui-state-hover" );
+	}
+);
+</script>
+	
+	
+	
+</body>
+</html>
